04b3dab2a1bea6db2fd7fb9d4550c64d423801a7
Peter <szpeto12@gmail.com>
2025-08-06 16:11:28 +0200

    Version v0.1.1.4 - Add ability to store multiple tweets locally until refresh

--- frontend/src/components/Tweet/Tweet.jsx
+++ frontend/src/components/Tweet/Tweet.jsx
@@ -8,16 +8,21 @@ function Tweet(){
     /* useState keeps the tweet stored across multiple renders, after page refresh, the entire app runs from beginning, 
        changing the tweet to its default state */
     const [tweet, setTweet] = useState("Tweet me!");
+    const [tweets, setTweets] = useState([]);
 
-    // Only for experimenting purposes (so far)
-    function handleClick(e){
-        e.target.textContent = "Tweet tweet";
+    function handleClick(){
+        // We do not need an event, since onChange on TextArea updates the current tweet on every change
+        let items = tweets;
+        items.push(tweet);
+        setTweets(items);
+
+        console.log(tweets);
     }
 
     return (
         <div className="tweet-container">
             <TextArea value={ tweet } onChange={ setTweet } />
-            <Button text="Submit" type="submit" onClick={ (e) => handleClick(e) } />
+            <Button text="Submit" type="submit" onClick={ () => handleClick() } />
             <Button text="Clear text" type="info" onClick={ () => setTweet("") } />
         </div>
     );
