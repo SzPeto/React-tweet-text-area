c48d98b14e82c2d9186a543ebab20f7e90d85178
Peter <szpeto12@gmail.com>
2025-08-07 23:23:11 +0200

    Version v0.5.5.3

--- backend/src/tweets/tweets.controller.ts
+++ backend/src/tweets/tweets.controller.ts
@@ -1,13 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { TweetsService } from './tweets.service';
+import { Controller, Get, Post } from "@nestjs/common";
+import { TweetsService } from "./tweets.service";
 
 @Controller('tweets')
 export class TweetsController {
 
+    // NestJS automatic DI via constructor
     private readonly tweetService: TweetsService; // Readonly is like const, but for instances
 
     constructor(tweetsService: TweetsService){
         this.tweetService = tweetsService;        
+    }
+
+    @Get()
+    getAllTweets(){
+        return this.tweetService.getAllTweets();
+    }
+
+    @Post()
+    addTweet(){
+
     }
 
 }
--- backend/src/tweets/tweets.service.ts
+++ backend/src/tweets/tweets.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TweetsService {}
+export class TweetsService {
+
+    private tweets: [];
+
+    getAllTweets(){
+        return this.tweets;
+    }
+
+}
