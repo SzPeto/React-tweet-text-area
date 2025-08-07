b96514c8c288a667e8d46ff99b84aac78fb97e68
Peter <szpeto12@gmail.com>
2025-08-07 19:30:42 +0200

    Version v0.5.2.0 - Establish communication between FE and BE

--- tweet-api/src/main.ts
+++ tweet-api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // Be sure to put it before the app begins to listen
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
