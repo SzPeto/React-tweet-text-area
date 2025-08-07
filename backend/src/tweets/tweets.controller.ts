3c00cb2b67322a10de173bd483a70d74c059b1d1
Peter <szpeto12@gmail.com>
2025-08-07 23:02:36 +0200

    Version v0.5.5.2

--- backend/src/tweets/tweets.controller.ts
+++ backend/src/tweets/tweets.controller.ts
@@ -4,8 +4,10 @@ import { TweetsService } from './tweets.service';
 @Controller('tweets')
 export class TweetsController {
 
+    private readonly tweetService: TweetsService; // Readonly is like const, but for instances
+
     constructor(tweetsService: TweetsService){
-        
+        this.tweetService = tweetsService;        
     }
 
 }
