cd79365348e50f861b46c8739e6bce0c29f784da
Peter <szpeto12@gmail.com>
2025-08-14 11:54:02 +0200

    v0.8.3.5 - Minor changes

--- frontend/src/components/tweets/tweet-list/TweetList.tsx
+++ frontend/src/components/tweets/tweet-list/TweetList.tsx
@@ -1,24 +1,28 @@
 import './TweetList.css'
 import Tweet from '@/components/tweets/tweet/Tweet.tsx'
 
-type Tweet = {
+type TweetType = {
   id: number,
   content: string
 }
 
 type TweetListProps = {
-  tweets: Tweet[]
+  tweets: TweetType[]
 }
 
 const TweetList = (props: TweetListProps) => {
 
   return (
     <div className='tweet-list-container-l2'>
-      {
-        Array.isArray(props.tweets) && props.tweets.length > 0 ? 
-        props.tweets.map((tweet) => (<Tweet id={ tweet.id } content={ tweet.content } />)) : 
-        'There are no tweets available'
-      }
+      {Array.isArray(props.tweets) && props.tweets.length > 0 ? 
+        props.tweets.map((tweet) => (
+          <Tweet 
+            id={ tweet.id } 
+            content={ tweet.content } 
+            key={ tweet.id } 
+          />
+        )) : 
+        'There are no tweets available'}
     </div>
     
   )
