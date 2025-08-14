9f395835001402cad822e4ab2b988b7c05aa9346
Peter <szpeto12@gmail.com>
2025-08-14 21:38:34 +0200

    v0.8.3.8 - Install Mongoose, establish connection with MongoDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TweetsModule } from './tweets/tweets.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TweetsModule],
+  imports: [
+    TweetsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/tweetsdb')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
