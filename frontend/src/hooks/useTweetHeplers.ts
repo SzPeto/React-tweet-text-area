dd1c788cb432cccd0bb2255a54ee0d3c7cb09e59
Peter <szpeto12@gmail.com>
2025-08-17 10:36:15 +0200

    v0.8.6.7 - Add exception handling to fetch functions

--- frontend/src/hooks/useTweetHeplers.ts
+++ frontend/src/hooks/useTweetHeplers.ts
@@ -1,55 +1,86 @@
+import type { Tweet } from '@/types/tweet.ts'
 
 export const useTweetHelpers = () => {
-    
+  
   async function fetchFromBe(){
-    const response = await fetch('/api/tweets')
-    const json = await response.json()
+    let json: Tweet[] = []
+    try{
+      const response = await fetch('/api/tweets')
+      json = await response.json()
+    }catch(err){
+      console.log('Error during GET request from backend : ', err)
+    }
     return json
   }
   
   async function sendToBe(tweet: string, dateSubmitted: string){
+
+    let json: Tweet[] = []
     const postMessage = {
       content: tweet,
       dateSubmitted: dateSubmitted
     }
 
-    const response = await fetch('/api/tweets', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(postMessage),
-    })
-    const json = await response.json()
+    try{
+      const response = await fetch('/api/tweets', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(postMessage),
+      })
+      json = await response.json()
+    }catch(err){
+      console.log('Error during POST request to backend : ', err)
+    }
     return json
   }
 
   async function deleteAll(){
-    const response = await fetch('/api/tweets', {
-      method: 'DELETE',
-    })
-    const json = await response.json()
+
+    let json: Tweet[] = []
+
+    try{
+      const response = await fetch('/api/tweets', {
+        method: 'DELETE',
+      })
+      json = await response.json()
+    }catch(err){
+      console.log('Error during DELETE request ( deleteAll() ) : ', err)
+    }
     return json
   }
 
   async function deleteOne(id: string){
-    const response = await fetch(`/api/tweets/${id}`, {
-      method: 'DELETE',
-    })
-    const json = await response.json()
+
+    let json: Tweet[] = []
+
+    try{
+      const response = await fetch(`/api/tweets/${id}`, {
+        method: 'DELETE',
+      })
+      json = await response.json()
+    }catch(err){
+      console.log('Error during DELETE request ( deleteOne() ) : ', err)
+    }
     return json
   }
 
   async function updateOne(id: string, tweet: string){
 
+    let json: Tweet[] = []
     const updateMessage = {
       content: tweet
     }
 
-    const response = await fetch(`/api/tweets/${id}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(updateMessage),
-    })
-    const json = await response.json()
+    try{
+      const response = await fetch(`/api/tweets/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updateMessage),
+      })
+      json = await response.json()
+    }catch(err){
+      console.log('Error during PATCH request : ', err)
+    }
     return json
   }
 
