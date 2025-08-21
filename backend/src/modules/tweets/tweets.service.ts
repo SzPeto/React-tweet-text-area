0061e906883c64e413f3af1070910b9f6b21725e
Peter <szpeto12@gmail.com>
2025-08-21 14:28:36 +0200

    v0.8.8.9 - Comment out prettier in eslint, BE

--- backend/src/modules/tweets/tweets.service.ts
+++ backend/src/modules/tweets/tweets.service.ts
@@ -23,9 +23,9 @@ export class TweetsService {
 
   async getTweetById(id: string) {
     // Ensure to throw NotFoundException if the id isn't valid
-    if(!Types.ObjectId.isValid(id)) throw new NotFoundException(`Invalid Tweet ID : ${id}`)
+    if (!Types.ObjectId.isValid(id)) throw new NotFoundException(`Invalid Tweet ID : ${id}`)
     const tweet = await this.tweetModel.findById(id)
-    if(!tweet) throw new NotFoundException(`Tweet with ID : ${id} not found!`)
+    if (!tweet) throw new NotFoundException(`Tweet with ID : ${id} not found!`)
     return tweet
   }
 
@@ -49,9 +49,9 @@ export class TweetsService {
   */
 
   async replaceTweetById(id: string, updateTweetDto: UpdateTweetDto) {
-    if(!Types.ObjectId.isValid(id)) throw new NotFoundException(`Invalid Tweet ID : ${id}`)
+    if (!Types.ObjectId.isValid(id)) throw new NotFoundException(`Invalid Tweet ID : ${id}`)
     const updated = await this.tweetModel.findByIdAndUpdate(id, {$set: updateTweetDto}, {new: true}).exec()
-    if(!updated) throw new NotFoundException(`Error during updating, tweet with ID : ${id} not found`)
+    if (!updated) throw new NotFoundException(`Error during updating, tweet with ID : ${id} not found`)
     return updated
   }
 
@@ -69,7 +69,7 @@ export class TweetsService {
   async deleteTweetById(id: string) {
     if (!Types.ObjectId.isValid(id)) throw new NotFoundException(`Invalid Tweet ID : ${id}`)
     const result = await this.tweetModel.deleteOne({ _id: id })
-    if(result.deletedCount === 0) throw new NotFoundException(`Tweet with ID : ${id} not found!`)
+    if (result.deletedCount === 0) throw new NotFoundException(`Tweet with ID : ${id} not found!`)
     return result
   }
 }
