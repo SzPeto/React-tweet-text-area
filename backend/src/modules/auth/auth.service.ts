1e6d76d58528ae3e98c2b7319682fd6c1d82f6a2
Peter <szpeto12@gmail.com>
2025-08-24 00:22:08 +0200

    v0.9.1.3 - Login system underway v6

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common'
-import { UsersService } from '../users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
 
   constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
-  async validateUser(userName: string, password: string) {
-    
+  async validateUser(userName: string, pw: string) {
+    const user = await this.usersService.findUserByName(userName)
+    if (!user) return null
+    const passwordMatch = await bcrypt.compare(pw, user.password)
+    if (!passwordMatch) return null
+    // user.toObject() checks if it is a Mongoose document, if yes, it converts it to plain TS object, if not return user
+    // The destructuring extracts the password and the left fields besides password separately, e.g. : 
+    // safeFields = { username: "peter", email: "a@b.com" }
+    const { password, ...safeFields } = user.toObject ? user.toObject() : user
+    return safeFields
+  }
+
+  async login(user: any) {
+    // ?? Nullish coalescing operator, use 'a' if not null or undefined, otherwise use 'b'
+    const payload = { username: user.userName, sub: user._id ?? user.id }
+    return { access_token: this.jwtService.sign(payload) }
   }
 
 }
