d614676c433fd645fb6e97dcdb54f41f6d0ea08e
Peter <szpeto12@gmail.com>
2025-08-23 21:07:01 +0200

    v0.9.0.9 - Login system underway v3

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller('users')
 export class UsersController {
   
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    await this.usersService.createUser(createUserDto)
+  }
+
 }
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from "@nestjs/common";
-import { User, UserDocument } from "./schemas/users.schema";
-import { Model } from "mongoose";
-import { InjectModel } from "@nestjs/mongoose";
-import { CreateUserDto } from "./dto/create-user.dto";
+import { ConflictException, Injectable } from '@nestjs/common'
+import { User, UserDocument } from './schemas/users.schema'
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { CreateUserDto } from './dto/create-user.dto'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
 
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  createUser(createUserDto: CreateUserDto) {
-    const userExist = this.userModel.findOne({ userName: createUserDto.userName })
+  async createUser(createUserDto: CreateUserDto) {
+    const userExist = await this.userModel.findOne({ userName: createUserDto.userName })
+    if (userExist) throw new ConflictException(`User with name ${ createUserDto.userName } already exists!`)
+    const hashedPw = bcrypt
   }
 
 }
