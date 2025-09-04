bf0789dd7a1f72ee45b3e8cdd025a3d7700326e6
Peter <szpeto12@gmail.com>
2025-09-04 16:54:59 +0200

    v0.9.9.8.8 - Working on one to many relationship v1 - WIP

--- backend/src/modules/tweets/schemas/tweet.schema.ts
+++ backend/src/modules/tweets/schemas/tweet.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { HydratedDocument } from 'mongoose'
+import mongoose, { HydratedDocument } from 'mongoose'
+import { User } from 'src/modules/users/schemas/users.schema'
 
 export type TweetDocument = HydratedDocument<Tweet>
 
@@ -7,6 +8,10 @@ export type TweetDocument = HydratedDocument<Tweet>
 export class Tweet {
   @Prop({ required: true, trim: true })
   content: string
+
+  // means in MongoDB, this field is stored as an ObjectId (basically just the user’s _id).
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  user: User
 }
 
 export const TweetSchema = SchemaFactory.createForClass(Tweet)
